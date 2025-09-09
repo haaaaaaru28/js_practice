e9742ed56913dc8f64958c6cf03dd51e907ec986
haaaaaaru28 <hanna.anand06@gmail.com>
2025-09-09 07:01:58 +0000

    add

---
+++ basics/06_datatype_conversion.js
@@ -0,0 +1,24 @@
+let score = "33abc" 
+// console.log(typeof(score));
+
+let valueInnUMBER = Number(score)
+// console.log(typeof(valueInnUMBER));
+// console.log(valueInnUMBER)
+// ismai value convert nhi huyi h par still number show ho rha h iska dhyan rakhna h
+// console.log(typeof(NULL));
+// console.log(typeof(undefined));
+
+
+// when we convert "33" --> can we converted
+//"33abc" --> NaN
+// true ==> 1; false==> 0 
+
+let isLoggedIn = "hitesh"
+let booleanisloggedin = Boolean(isLoggedIn)
+// console.log(booleanisloggedin);
+
+// 1 ==> true; 0 ==> false
+// "' ==> false
+//"hanna"==>true
+
+
